fe1e58f2425527f7e5f1777a91737eab9dcff5f6
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-12-22 20:02:22 +0300

    feat: add some tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -462,8 +462,23 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  if (iterations === 0) {
+    return str;
+  }
+  let startStr = '';
+  let finishStr = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (i % 2 === 0) {
+      startStr += str[i];
+    } else {
+      finishStr += str[i];
+    }
+  }
+  const commonStr = startStr + finishStr;
+  return iterations - 1 !== 0
+    ? shuffleChar(commonStr, iterations - 1)
+    : commonStr;
 }
 
 /**
